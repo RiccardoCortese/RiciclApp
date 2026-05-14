0666cc9f28044dab0e253213daacefccd4b292b4
Riccardo <r.riccardocortese@gmail.com>
2026-05-14 11:15:57 +0200

    implementazione api elimina account

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -34,4 +34,25 @@ router.get('/profile', authMiddleware, async (req, res) => {
     }
 });
 
+// Rotta protetta per eliminare l'account dell'utente loggato con token JWT
+router.delete('/delete', authMiddleware, async (req, res) => {
+    try {
+        // req.user.userId arriva dal token JWT verificato nel middleware
+        const user = await User.findById(req.user.userId);
+
+        if (!user) {
+            return res.status(404).json({message: "Utente non trovato"});
+        }
+
+        // Elimina l'utente dal database, funzione findByIdAndDelete è più efficiente perché elimina direttamente senza dover prima recuperare l'utente
+        //è una funzione di mongoose
+        await User.findByIdAndDelete(req.user.userId); 
+
+        res.status(200).json({message: "Account eliminato con successo"});
+    } catch (error) {
+        console.error('Errore durante l\'eliminazione dell\'account:', error);
+        res.status(500).json({message: 'Errore del server'});
+    }
+});
+
 module.exports = router;
