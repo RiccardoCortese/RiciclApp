d22fba7ace047d6d616e00fef9c31eb7b23402cb
Riccardo <r.riccardocortese@gmail.com>
2026-05-17 13:55:18 +0200

    fix bugs elimina account

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -5,6 +5,7 @@ const User = require('../models/user');
 const authMiddleware = require('../middleware/authMiddleware');
 const bcrypt = require('bcryptjs');
 const { sendPasswordResetEmail } = require('../services/email_verification');
+const { sendAccountDeletionEmail } = require('../services/email_elimina_account');
 
 // Rotta protetta per ottenere i dati dell'utente loggato
 router.get('/profile', authMiddleware, async (req, res) => {
@@ -204,5 +205,30 @@ router.post('/profile/reset-password', authMiddleware, async (req, res) => {
         res.status(500).json({ message: 'Errore del server' });
     }
 });
+
+// Rotta protetta per eliminare l'account dell'utente loggato con token JWT
+router.delete('/delete', authMiddleware, async (req, res) => {
+    try {
+        // req.user.userId arriva dal token JWT verificato nel middleware
+        const user = await User.findById(req.user.userId);
+
+        if (!user) {
+            return res.status(404).json({message: "Utente non trovato"});
+        }
+
+        // Elimina l'utente dal database, funzione findByIdAndDelete è più efficiente perché elimina direttamente senza dover prima recuperare l'utente
+        //è una funzione di mongoose
+        await User.findByIdAndDelete(req.user.userId); 
+
+        // Invia l'email di eliminazione account
+        await sendAccountDeletionEmail({ username: user.name, email: user.email });
+
+        res.status(200).json({message: "Account eliminato con successo"});
+    } catch (error) {
+        console.error('Errore durante l\'eliminazione dell\'account:', error);
+        res.status(500).json({message: 'Errore del server'});
+    }
+});
+
 
 module.exports = router;
