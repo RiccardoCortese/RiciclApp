a1fb9cd0cd3ee955d44e02895e46a3bd1770f724
Riccardo <r.riccardocortese@gmail.com>
2026-06-10 20:11:25 +0200

    modificato: accesso per render

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,6 +1,5 @@
 import { Platform } from 'react-native';
 
-
 export const API_URL = Platform.OS === 'web'
-  ? 'http://localhost:5000/api'
-  : 'http://192.168.1.112:5000/api';
+  ? 'https://riciclapp-backend.onrender.com/api'
+  : 'https://riciclapp-backend.onrender.com/api';
